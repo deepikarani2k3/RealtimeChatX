ae8ddb74652f9e2d1ac8dd7a445a91c0201ec636
Deepika Rani <deepikasingh3729@gmail.com>
2025-07-18 12:28:36 +0530

    ⚙️ Initialized Express backend and routed login/register pages

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const cors = require('cors');
 require('dotenv').config();
 
@@ -6,11 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Sample test route
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.get('/', (req, res) => {
-  res.send('API is working ✅');
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 app.listen(PORT, () => {
